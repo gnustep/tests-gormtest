96931f4866ebcf496c0bfaa088735cca1174dcfa
Gregory John Casamento <greg.casamento@gmail.com>
2005-03-05 11:15:07 +0000

    Testing encoding to make certain objects are not encoded twice.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/gormtest/trunk@20847 72102866-910b-0410-8b05-ffd578937521

--- GSTextView.m
+++ GSTextView.m
@@ -1,6 +1,12 @@
 #include "GSTextView.h"
 
 @implementation GSTextView
+- (id) initWithFrame: (NSRect)frame
+{
+  NSLog(@"Called designated init %@",self);
+  return [super initWithFrame: frame];
+}
+
 - (id) initWithCoder: (NSCoder *)coder
 {
   NSLog(@"Decoding custom class %@",NSStringFromClass([self class]));
