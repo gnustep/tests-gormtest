dee93b4676f98595caa47bd282c66d4e38ffc238
Gregory John Casamento <greg.casamento@gmail.com>
2005-05-03 03:59:06 +0000

    GSWindow designated init added.  Also changed .gorm files to add new window.
    git-svn-id: svn+ssh://svn.gna.org/svn/gnustep/tests/gormtest/trunk@21181 72102866-910b-0410-8b05-ffd578937521

--- GSWindow.m
+++ GSWindow.m
@@ -1,6 +1,17 @@
 #include "GSWindow.h"
 
 @implementation GSWindow
+- (id)initWithContentRect:(NSRect)contentRect
+		styleMask:(unsigned int)styleMask
+		  backing:(NSBackingStoreType)bufferingType
+		    defer:(BOOL)flag
+{
+  // It was neccesary to override this method to get the borderless
+  // window since it is not available from InterfaceBuilder.
+  NSLog(@"GSWindow class has been instantiated... -- DESIGNATED INIT CALLED.");
+  return self;
+}
+
 - (id) initWithCoder: (NSCoder *)coder
 {
   NSLog(@"Decoding custom class %@",NSStringFromClass([self class]));
